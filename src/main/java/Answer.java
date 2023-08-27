e4cae3c79b9738739ed51bdfdf37445560ebe6a9
vsvarost <vsvarost@gmail.com>
2023-08-27 22:38:53 +0300

    Task 1.1 Done

---
+++ src/main/java/Answer.java
@@ -0,0 +1,19 @@
+public class Answer {
+    public static void arrayOutOfBoundsException() {
+        // Напишите свое решение ниже
+        int[] arr = new int[2];
+        int temp = arr[8];
+
+    }
+
+    public static void divisionByZero() {
+        // Напишите свое решение ниже
+        int divRez = 15/0;
+    }
+
+    public static void numberFormatException() {
+        // Напишите свое решение ниже
+        String str = "Some string...";
+        int value = Integer.parseInt(str);
+    }
+}
