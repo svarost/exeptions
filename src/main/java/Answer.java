4ed397bbf9db5f4d61baa58cefb19999c5ad9dee
vsvarost <vsvarost@gmail.com>
2023-09-02 22:44:28 +0300

    1.3 Done

--- src/main/java/Answer.java
+++ src/main/java/Answer.java
@@ -1,2 +1,20 @@
-public class Answer {
+import java.util.Arrays;
+
+class Answer {
+    public int[] divArrays(int[] a, int[] b) {
+        // ??????? ???? ??????? ????
+        for (int item : b) {
+            if (item == 0)
+                throw new RuntimeException("Ошибка деления на ноль.");
+        }
+
+         if (a.length != b.length || a == null || b == null)
+            return new int[]{0};
+        else {
+            int[] c = new int[a.length];
+            for (int i = 0; i < a.length; i++)
+                c[i] = a[i] / b[i];
+            return c;
+        }
+    }
 }
--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,22 @@
-public class Printer {
+import java.util.Arrays;
+
+public class Printer{
+    public static void main(String[] args) {
+        int[] a = {};
+        int[] b = {};
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = new int[]{12, 8, 16};
+            b = new int[]{4, 2, 0};
+        }
+        else{
+            a = Arrays.stream(args[0].split(", ")).mapToInt(Integer::parseInt).toArray();
+            b = Arrays.stream(args[1].split(", ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        Answer ans = new Answer();
+        String itresume_res = Arrays.toString(ans.divArrays(a, b));
+        System.out.println(itresume_res);
+    }
 }
